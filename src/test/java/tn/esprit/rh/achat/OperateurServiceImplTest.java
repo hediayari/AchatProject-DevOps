59b9770ee6e2ec9fa15eb81a5718ca3c27920903
Ayari Hedi <mohamedhedi.ayari@esprit.tn>
2023-11-03 01:08:22 +0100

    fichier Test

--- src/test/java/tn/esprit/rh/achat/OperateurServiceImplTest.java
+++ src/test/java/tn/esprit/rh/achat/OperateurServiceImplTest.java
@@ -1,3 +1,5 @@
+package tn.esprit.rh.achat;
+
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
@@ -5,6 +7,9 @@ import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.Mockito;
 import org.mockito.junit.jupiter.MockitoExtension;
+import tn.esprit.rh.achat.entities.Operateur;
+import tn.esprit.rh.achat.repositories.OperateurRepository;
+import tn.esprit.rh.achat.services.OperateurServiceImpl;
 
 @ExtendWith(MockitoExtension.class)
 public class OperateurServiceImplTest {
@@ -31,4 +36,24 @@ public class OperateurServiceImplTest {
         Assertions.assertEquals(mockOperateur.getIdOperateur(), retrievedOperateur.getIdOperateur());
         Assertions.assertEquals(mockOperateur.getNom(), retrievedOperateur.getNom());
     }
+    @Test
+    public void testAddOperateur() {
+        Operateur newOperateur = new Operateur();
+        newOperateur.setNom("NewNom");
+        newOperateur.setPrenom("NewPrenom");
+        newOperateur.setPassword("NewPassword");
+
+        // Mock the save method to return the same object that's passed to it
+        Mockito.when(operateurRepository.save(newOperateur)).thenReturn(newOperateur);
+
+        // Invoke the addOperateur method of operateurService
+        Operateur addedOperateur = operateurService.addOperateur(newOperateur);
+
+        // Assertions
+        Assertions.assertNotNull(addedOperateur);
+        Assertions.assertEquals(newOperateur.getNom(), addedOperateur.getNom());
+        Assertions.assertEquals(newOperateur.getPrenom(), addedOperateur.getPrenom());
+        Assertions.assertEquals(newOperateur.getPassword(), addedOperateur.getPassword());
+    }
+
 }
---
+++ src/test/java/tn/esprit/rh/achat/ProduitServiceTest.java
@@ -0,0 +1,71 @@
+package tn.esprit.rh.achat;
+
+
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.Optional;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.mockito.*;
+import tn.esprit.rh.achat.entities.Produit;
+import tn.esprit.rh.achat.repositories.ProduitRepository;
+import tn.esprit.rh.achat.services.ProduitServiceImpl;
+
+public class ProduitServiceTest {
+
+    @InjectMocks
+    private ProduitServiceImpl produitService;
+
+    @Mock
+    private ProduitRepository produitRepository;
+
+    @BeforeEach
+    public void init() {
+        MockitoAnnotations.openMocks(this);
+    }
+
+    @Test
+    public void testRetrieveAllProduits() {
+        Produit produit1 = new Produit();
+        produit1.setIdProduit(1L);
+
+        Produit produit2 = new Produit();
+        produit2.setIdProduit(2L);
+
+        Mockito.when(produitRepository.findAll()).thenReturn(Arrays.asList(produit1, produit2));
+
+        List<Produit> result = produitService.retrieveAllProduits();
+        Assertions.assertNotNull(result);
+        Assertions.assertEquals(2, result.size());
+    }
+
+    @Test
+    public void testAddProduit() {
+        Produit produit = new Produit();
+        produit.setIdProduit(1L);
+        produit.setCodeProduit("PROD001");
+
+        Mockito.when(produitRepository.save(ArgumentMatchers.any(Produit.class))).thenReturn(produit);
+
+        Produit result = produitService.addProduit(produit);
+        Assertions.assertNotNull(result);
+        Assertions.assertEquals(produit.getCodeProduit(), result.getCodeProduit());
+    }
+
+    @Test
+    public void testRetrieveProduit() {
+        Produit produit = new Produit();
+        produit.setIdProduit(1L);
+
+        Mockito.when(produitRepository.findById(1L)).thenReturn(Optional.of(produit));
+
+        Produit result = produitService.retrieveProduit(1L);
+        Assertions.assertNotNull(result);
+        Assertions.assertEquals(produit.getIdProduit(), result.getIdProduit());
+    }
+
+    // ... Ajouter d'autres tests pour les autres méthodes ...
+}
