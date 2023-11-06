fa0fc69341567e3400dc9786b2dec2dee3ff3a99
Ayari Hedi <mohamedhedi.ayari@esprit.tn>
2023-11-06 17:47:17 +0100

    cros mapping

--- src/main/java/tn/esprit/rh/achat/AchatApplication.java
+++ src/main/java/tn/esprit/rh/achat/AchatApplication.java
@@ -2,7 +2,10 @@ package tn.esprit.rh.achat;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 import org.springframework.scheduling.annotation.EnableScheduling;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @EnableScheduling
 @SpringBootApplication
@@ -12,4 +15,17 @@ public class AchatApplication {
         SpringApplication.run(AchatApplication.class, args);
     }
 
+    @Bean
+    public WebMvcConfigurer corsConfigurer() {
+        return new WebMvcConfigurer() {
+            @Override
+            public void addCorsMappings(CorsRegistry registry) {
+                registry.addMapping("/**") // You can specify the path patterns here
+                        .allowedOrigins("*") // Specify the allowed origins here
+                        .allowedMethods("GET", "POST", "PUT", "DELETE", "PATCH") // Allowed HTTP methods
+                        .allowedHeaders("*") // Allowed headers
+                        .allowCredentials(true); // Credential support
+            }
+        };
+    }
 }
