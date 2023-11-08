1416a7ed1802a27342fcafe3fd877f06e4407ece
oubaidhl <hlaymiabid@gmail.com>
2023-11-08 01:59:26 +0100

    test junit and mockito

--- src/main/java/tn/esprit/rh/achat/controllers/StockRestController.java
+++ src/main/java/tn/esprit/rh/achat/controllers/StockRestController.java
@@ -18,7 +18,6 @@ public class StockRestController {
 	@Autowired
 	IStockService stockService;
 
-	// http://localhost:8089/SpringMVC/stock/retrieve-all-stocks
 	@GetMapping("/retrieve-all-stocks")
 	@ResponseBody
 	public List<Stock> getStocks() {
@@ -26,14 +25,12 @@ public class StockRestController {
 		return list;
 	}
 
-	// http://localhost:8089/SpringMVC/stock/retrieve-stock/8
 	@GetMapping("/retrieve-stock/{stock-id}")
 	@ResponseBody
 	public Stock retrieveStock(@PathVariable("stock-id") Long stockId) {
 		return stockService.retrieveStock(stockId);
 	}
 
-	// http://localhost:8089/SpringMVC/stock/add-stock
 	@PostMapping("/add-stock")
 	@ResponseBody
 	public Stock addStock(@RequestBody Stock s) {
@@ -41,34 +38,16 @@ public class StockRestController {
 		return stock;
 	}
 
-	// http://localhost:8089/SpringMVC/stock/remove-stock/{stock-id}
 	@DeleteMapping("/remove-stock/{stock-id}")
 	@ResponseBody
 	public void removeStock(@PathVariable("stock-id") Long stockId) {
 		stockService.deleteStock(stockId);
 	}
 
-	// http://localhost:8089/SpringMVC/stock/modify-stock
 	@PutMapping("/modify-stock")
 	@ResponseBody
 	public Stock modifyStock(@RequestBody Stock stock) {
 		return stockService.updateStock(stock);
 	}
-
-	/*
-	 * Spring Scheduler : Comparer QteMin tolérée (à ne pa dépasser) avec
-	 * Quantité du stock et afficher sur console la liste des produits inférieur
-	 * au stock La fct schédulé doit obligatoirement etre sans paramètres et
-	 * sans retour (void)
-	 */
-	// http://localhost:8089/SpringMVC/stock/retrieveStatusStock
-	// @Scheduled(fixedRate = 60000)
-	// @Scheduled(fixedDelay = 60000)
-	//@Scheduled(cron = "*/60 * * * * *")
-	//@GetMapping("/retrieveStatusStock")
-//	@ResponseBody
-//	public void retrieveStatusStock() {
-//		stockService.retrieveStatusStock();
-//	}
 
 }
