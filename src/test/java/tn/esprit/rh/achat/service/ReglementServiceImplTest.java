3046c2c8e8c89fdaba176e3bff737d9bad5ce2df
ladhari-youssef <youssefladhari.r@gmail.com>
2023-11-08 13:35:16 +0100

    eddit

--- src/test/java/tn/esprit/rh/achat/service/ReglementServiceImplTest.java
+++ src/test/java/tn/esprit/rh/achat/service/ReglementServiceImplTest.java
@@ -11,6 +11,7 @@ import tn.esprit.rh.achat.repositories.ReglementRepository;
 import tn.esprit.rh.achat.services.ReglementServiceImpl;
 
 import java.util.ArrayList;
+import java.util.Date;
 import java.util.List;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
@@ -26,19 +27,32 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 
     @BeforeEach
     void setUp() {
-        // Define the behavior of the mock repository
         List<Reglement> reglements = new ArrayList<>();
-        reglements.add(new Reglement(/* Initialize with appropriate values */));
-        reglements.add(new Reglement(/* Initialize with appropriate values */));
+        Reglement reglement1 = new Reglement();
+        reglement1.setIdReglement(1L);
+        reglement1.setMontantPaye(100.0f);
+        reglement1.setMontantRestant(50.0f);
+        reglement1.setPayee(false);
+        reglement1.setDateReglement(new Date());
+
+        Reglement reglement2 = new Reglement();
+        reglement2.setIdReglement(2L);
+        reglement2.setMontantPaye(200.0f);
+        reglement2.setMontantRestant(75.0f);
+        reglement2.setPayee(true);
+        reglement2.setDateReglement(new Date());
+
+        reglements.add(reglement1);
+        reglements.add(reglement2);
 
         Mockito.when(reglementRepository.findAll()).thenReturn(reglements);
+
     }
 
     @Test
      void testRetrieveAllReglements() {
         List<Reglement> retrievedReglements = reglementService.retrieveAllReglements();
 
-        // Assert that the method returns the list of Reglements as expected
-        assertEquals(2, retrievedReglements.size()); // Adjust the expected size accordingly
+        assertEquals(2, retrievedReglements.size());
     }
 }
